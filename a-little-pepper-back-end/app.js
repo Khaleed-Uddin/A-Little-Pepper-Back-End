5a55c75a58af67c088e5c72db963969aa742a8ea
Khaleed Uddin <uddin1786@gmail.com>
2022-11-16 19:47:50 -0500

    acutally installed the dependicies

--- a-little-pepper-back-end/app.js
+++ a-little-pepper-back-end/app.js
@@ -6,9 +6,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const foodController = require('./controllers/foodController');
-// app.use('/foods', foodController);
-
 app.get('/', (req, res) => {
     res.send(`Welcome to A Little Pepper!!`)
 });
